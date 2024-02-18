184f9238961f2f76a8691c17e639ca46e7e50247
Glen Rutherford <glenrutherford4@gmail.com>
2024-02-19 09:10:23 +1100

    WIP Time Based Levels added useful links as comments

--- Pine scripts/Time Based Levels.pine.js
+++ Pine scripts/Time Based Levels.pine.js
@@ -4,10 +4,20 @@
 //@version=5
 indicator("Time Based Levels", overlay = true)
 
-londonOpenKillZone = time(timeframe.period, '0200-0500', 'UTC-5')
+plotPastDayHighLowCount = input.int(5, "Number of past days highs and lows to plot")
+
+// Do I even need this when I can use LuxAlgo's ICT Killzones indicator?
+// londonOpenKillZone = time(timeframe.period, '0200-0500', 'UTC-5')
+// plot(londonOpenKillZone)
+
 
 [dailyTimeFrameHigh, dailyTimeFrameLow] = request.security("","1D", [high, low])
 
+// How to get bar_index for start of each day?
+// https://www.tradingcode.net/tradingview/daily-high-low-boxes/
+// https://www.tradingcode.net/tradingview/session-high-low-box/
+// line.new(x1=bar_index, y1=open, x2=bar_index+lineLength, y2=open, color= color.blue)
 
 
 plot(dailyTimeFrameHigh)
+plot(dailyTimeFrameLow)
