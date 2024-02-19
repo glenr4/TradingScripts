e00a46bcccf72facdc7b4a81d0582f3e88c8ea08
Glen Rutherford <glenrutherford4@gmail.com>
2024-02-19 12:45:28 +1100

    Time Based Levels works for high and low

--- Pine scripts/Time Based Levels.pine.js
+++ Pine scripts/Time Based Levels.pine.js
@@ -5,16 +5,15 @@
 indicator("Time Based Levels", overlay = true)
 
 extendDailyHighLowLength = input.int(48, "Number bars to extend daily high and low")
+dailyHighColour = input.color(color.aqua, "Daily high colour")
+dailyLowColour = input.color(color.blue, "Daily low colour")
 
 barCountPerDay = 24*60*60/timeframe.in_seconds()
-
-// Do I even need this when I can use LuxAlgo's ICT Killzones indicator?
-// londonOpenKillZone = time(timeframe.period, '0200-0500', 'UTC-5')
-// plot(londonOpenKillZone)
 
 [dailyHigh, dailyLow] = request.security("","1D", [high, low])
 
 if(ta.change(dailyHigh) and not barstate.islast) // does not draw for the current bar until it is closed ie not last
-    line.new(x1=bar_index - barCountPerDay, y1=dailyHigh, x2=bar_index+extendDailyHighLowLength, y2=dailyHigh, color= color.blue)
+    line.new(x1=bar_index - barCountPerDay, y1=dailyHigh, x2=bar_index+extendDailyHighLowLength, y2=dailyHigh, color= dailyHighColour)
 
-
+if(ta.change(dailyLow) and not barstate.islast) // does not draw for the current bar until it is closed ie not last
+    line.new(x1=bar_index - barCountPerDay, y1=dailyLow, x2=bar_index+extendDailyHighLowLength, y2=dailyLow, color= dailyLowColour)
