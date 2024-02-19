1400f9be93388854d1c0ae7c7816359e17c632e9
Glen Rutherford <glenrutherford4@gmail.com>
2024-02-19 12:39:58 +1100

    WIP Time Based Levels works for high

--- Pine scripts/Time Based Levels.pine.js
+++ Pine scripts/Time Based Levels.pine.js
@@ -4,20 +4,17 @@
 //@version=5
 indicator("Time Based Levels", overlay = true)
 
-plotPastDayHighLowCount = input.int(5, "Number of past days highs and lows to plot")
+extendDailyHighLowLength = input.int(48, "Number bars to extend daily high and low")
+
+barCountPerDay = 24*60*60/timeframe.in_seconds()
 
 // Do I even need this when I can use LuxAlgo's ICT Killzones indicator?
 // londonOpenKillZone = time(timeframe.period, '0200-0500', 'UTC-5')
 // plot(londonOpenKillZone)
 
+[dailyHigh, dailyLow] = request.security("","1D", [high, low])
 
-[dailyTimeFrameHigh, dailyTimeFrameLow] = request.security("","1D", [high, low])
-
-// How to get bar_index for start of each day?
-// https://www.tradingcode.net/tradingview/daily-high-low-boxes/
-// https://www.tradingcode.net/tradingview/session-high-low-box/
-// line.new(x1=bar_index, y1=open, x2=bar_index+lineLength, y2=open, color= color.blue)
+if(ta.change(dailyHigh) and not barstate.islast) // does not draw for the current bar until it is closed ie not last
+    line.new(x1=bar_index - barCountPerDay, y1=dailyHigh, x2=bar_index+extendDailyHighLowLength, y2=dailyHigh, color= color.blue)
 
 
-plot(dailyTimeFrameHigh)
-plot(dailyTimeFrameLow)
